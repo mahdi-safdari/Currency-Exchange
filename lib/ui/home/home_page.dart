5933b9d0a9fd7a46c222f37056cf5d9ad5b8bb48
Mehdi Safdari <mti.safdari@gmail.com>
2023-03-04 16:21:17 +0330

    marquee text & buy & sell buttons & Choice chip

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:exchange/ui/widgets/home_page_view.dart';
 import 'package:exchange/ui/widgets/theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,16 +13,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final PageController _pageViewController = PageController(initialPage: 0);
-
+  int defaultChoiceIndex = 0;
+  final List<String> choiceList = [
+    'Top MarketCaps',
+    'Top Gainers',
+    'Top Losers'
+  ];
   @override
   Widget build(BuildContext context) {
-    var primaryColor = Theme.of(context).primaryColor;
+    Color primaryColor = Theme.of(context).primaryColor;
     TextTheme textTheme = Theme.of(context).textTheme;
-    final List<String> choiceList = [
-      'Top MarketCaps',
-      'Top Gainers',
-      'Top Losers'
-    ];
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -69,6 +71,92 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ],
                   ),
+                ),
+              ),
+
+              //! marquee text
+              SizedBox(
+                height: 30,
+                width: MediaQuery.of(context).size.width - 30,
+                child: Marquee(
+                  text: '💵 Mahdi Safdari is Billionaire Dollar   ',
+                  scrollAxis: Axis.horizontal,
+                  style: textTheme.bodySmall,
+                  startAfter: const Duration(seconds: 3),
+                  pauseAfterRound: const Duration(seconds: 3),
+                ),
+              ),
+
+              //! buy & sell buttons
+              Padding(
+                padding: const EdgeInsets.only(right: 5, left: 5, top: 5),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(20),
+                          backgroundColor: Colors.green,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                          ),
+                        ),
+                        child: const Text('buy'),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          padding: const EdgeInsets.all(20),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                          ),
+                        ),
+                        child: const Text('sell'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //! Choice chip
+              Padding(
+                padding: const EdgeInsets.only(right: 5, left: 8, top: 10),
+                child: Row(
+                  children: <Widget>[
+                    Wrap(
+                      spacing: 8,
+                      children: List.generate(
+                        choiceList.length,
+                        (int index) {
+                          return ChoiceChip(
+                            label: Text(
+                              choiceList[index],
+                              style: textTheme.titleSmall,
+                            ),
+                            selected: defaultChoiceIndex == index,
+                            selectedColor: Colors.blue,
+                            onSelected: (bool value) {
+                              setState(
+                                () {
+                                  defaultChoiceIndex =
+                                      value ? index : defaultChoiceIndex;
+                                },
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
